b78a90b22b9178b700dd320e31f6468daf0bc070
Noah McKearney <noah.mckearney@snhu.edu>
2021-02-23 15:08:05 -0500

    Created Login and New User Screens Without CSS

---
+++ Messaging App/Models/CreateAccountModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MessagingApp.Models
+{
+    public class CreateAccountModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Phone_Number { get; set; }
+        public string Email { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Messaging App/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MessagingApp.Models
+{
+    public class LoginModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+    }
+}
