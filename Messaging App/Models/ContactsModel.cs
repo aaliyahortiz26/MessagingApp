aa320eecc69d15d497b4237a49fb6857a38b9cc4
Noah McKearney <noah.mckearney@snhu.edu>
2021-03-14 11:08:50 -0400

    Created contacts page

---
+++ Messaging App/Models/ContactsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.ComponentModel.DataAnnotations;
+
+namespace MessagingApp.Models
+{
+    public class ContactsModel : DBObject
+    {
+
+
+
+    }
+}
