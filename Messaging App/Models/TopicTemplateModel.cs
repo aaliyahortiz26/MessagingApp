6e2b90f0b372561d49ed0ebf3e5fb4619c538f93
Noah McKearney <noah.mckearney@snhu.edu>
2021-03-29 09:46:17 -0400

    created topicTemplate page

---
+++ Messaging App/Models/TopicTemplateModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.ComponentModel.DataAnnotations;
+
+namespace MessagingApp.Models
+{
+    public class TopicTemplateModel : DBObject
+    {
+        public static string m_topicName;
+
+
+        public string topicName { get=> m_topicName; set=>m_topicName = value; }
+    }
+
+}
