c5b10d1db72c6d27008008cab716b1e904d2c1fa
Tyler Scott <tyler.scott3@snhu.edu>
2021-04-01 12:44:03 -0400

    Topic Create works and I also added the button clear  I had made changes to both controllers and to html files

--- Messaging App/Models/DBManager.cs
+++ Messaging App/Models/DBManager.cs
@@ -6,8 +6,8 @@ using MySql.Data.MySqlClient;
 
 namespace MessagingApp.Models
 {
-    public class DBManager
-    {
+	public class DBManager
+	{
 		public List<Messages> GetMessagesGroup(string name)
 		{
 			DBObject.m_GroupName = name;
@@ -63,5 +63,62 @@ namespace MessagingApp.Models
 			}
 			return userGroupList;
 		}
+
+		public List<Messages> GetMessagesTopic(string name)
+		{
+			DBObject.m_TopicName = name;
+			List<string> messageDataList = new List<string>();
+			List<Messages> messageData = new List<Messages>();
+
+			const string connectionstring = "server=unitedmessaging.cylirx7dw3jb.us-east-1.rds.amazonaws.com;user id=Unitedmessaging; password = unitedmessaging21; persistsecurityinfo=True;database= united_messaging";
+			using (MySqlConnection conn = new MySqlConnection(connectionstring))
+			{
+				conn.Open();
+
+				MySqlCommand getMessagestopic = conn.CreateCommand();
+				getMessagestopic.CommandText = "SELECT userName, topics FROM messagetopicbase where topicName = @chatName"; // the command
+				getMessagestopic.Parameters.AddWithValue("@chatName", name);
+				getMessagestopic.ExecuteNonQuery();
+
+				// Execute the SQL command against the DB:
+				MySqlDataReader reader = getMessagestopic.ExecuteReader();
+				while (reader.Read())
+				{
+					messageDataList.Clear();
+					messageDataList.Add(Convert.ToString(reader[0]));
+					messageDataList.Add(Convert.ToString(reader[1]));
+					messageData.Add(new Messages(messageDataList));
+				}
+				reader.Close();
+				conn.Close();
+			}
+			return messageData;
+		}
+		public List<string> GettopicUsers(string name)
+		{
+			DBObject.m_TopicName = name;
+			List<string> userTopicList = new List<string>();
+
+			const string connectionstring = "server=unitedmessaging.cylirx7dw3jb.us-east-1.rds.amazonaws.com;user id=Unitedmessaging; password = unitedmessaging21; persistsecurityinfo=True;database= united_messaging";
+			using (MySqlConnection conn = new MySqlConnection(connectionstring))
+			{
+				conn.Open();
+
+				MySqlCommand getMessagestopic = conn.CreateCommand();
+				getMessagestopic.CommandText = "SELECT contactName FROM message where topicName = @chatName"; // the command
+				getMessagestopic.Parameters.AddWithValue("@chatName", name);
+
+				// Execute the SQL command against the DB:
+				MySqlDataReader reader = getMessagestopic.ExecuteReader();
+				while (reader.Read())
+				{
+					userTopicList.Add(Convert.ToString(reader[0]));
+				}
+				reader.Close();
+				conn.Close();
+			}
+			return userTopicList;
+		}
+
 	}
 }
