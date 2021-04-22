a75d94056b93b50273765976e43f2e67c388c267
Tyler Scott <tyler.scott3@snhu.edu>
2021-04-22 17:55:52 -0400

    Use This One I forgot to delete something off last one

--- Messaging App/Controllers/ChatController.cs
+++ Messaging App/Controllers/ChatController.cs
@@ -1121,7 +1121,7 @@ namespace MessagingApp.Controllers
 
                 conn2.Open();
 
-                string txtcmd2 = $"Insert into united_messaging.groupmessage (userid, chatName, contactName, userName, Invite, Sender)" + $"values ( @contactUserID, @groupChatContactTitle,@inviteUserContact, @userName, @Invite, @Sender) ";
+                string txtcmd2 = $"Insert into united_messaging.groupmessage (userid, chatName, contactName, userName, Invite)" + $"values ( @contactUserID, @groupChatContactTitle,@inviteUserContact, @userName, @Invite) ";
                 MySqlCommand cmd2 = new MySqlCommand(txtcmd2, conn2);
                 cmd2.CommandType = CommandType.Text;
                 cmd2.Parameters.AddWithValue("@contactUserID", id);
@@ -1129,7 +1129,6 @@ namespace MessagingApp.Controllers
                 cmd2.Parameters.AddWithValue("@inviteUserContact", DBObject.m_username);
                 cmd2.Parameters.AddWithValue("@userName",ic.inviteContact );
                 cmd2.Parameters.AddWithValue("@Invite", true);
-                cmd2.Parameters.AddWithValue("@Sender", false);
                 cmd2.ExecuteNonQuery();
                 conn2.Close();
 
